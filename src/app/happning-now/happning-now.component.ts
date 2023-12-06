43380e44b757e4d143d22c6cf012a95cafb8d083
shankar12np <114170724+shankar12np@users.noreply.github.com>
2023-12-06 14:17:40 -0500

    desin change

--- src/app/happning-now/happning-now.component.ts
+++ src/app/happning-now/happning-now.component.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./happning-now.component.css']
 })
 export class HappningNowComponent {
+
   events = [
+    {
+      title: 'TAMU DHEE VIRGINIA TAMU LOCHHAR',
+      description: 'Join us for an unforgettable cultural experience at TAMU DHEE VIRGINIA TAMU LOCHHAR. TAMU Lochchar is a traditional Nepali festival celebrated with great enthusiasm and joy. It is a time when the community comes together to showcase our rich cultural heritage through vibrant performances, delicious traditional cuisine, and engaging activities for all ages.\n' +
+       '\n\ ',
+
+      address: 'Hotel Hilton\n\\6550 Loisdale Rd\nSpringfield, VA 22150',
+      info: '$30, family-friendly event',
+
+    },
+
     {
       title: 'Holiday Walk of Lights',
       description: 'Holiday Walk of Lights is back this year and brighter than ever! Experience the magic of the holiday season as you walk down a path of unique, hand-crafted light displays at Neabsco Regional Park. Bring family and friends and be prepared to be in awe of the dazzling lights that surround us! Runs through December 23, 2023.',
